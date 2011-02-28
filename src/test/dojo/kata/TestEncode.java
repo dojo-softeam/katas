9624218f6896176f2a7af4caa3ff88ca41609743
Jean Helou <soft@byjean.eu>
2011-02-28 13:04:31 +0100

    java_kata

---
+++ src/test/dojo/kata/TestEncode.java
@@ -0,0 +1,13 @@
+package dojo.kata;
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class TestEncode{
+	@Test public void testtruth(){
+		assertTrue(true);
+	}
+}
