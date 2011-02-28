12bfbb35b8e603bb136d19d0aad808d97cf66261
Jean Helou <soft@byjean.eu>
2011-02-28 12:59:50 +0100

    ruby19_kata

---
+++ test.rb
@@ -0,0 +1,15 @@
+require 'test/unit'
+
+# Kata code goes after this line
+
+
+# Kata tests go after this line
+MiniTest::Unit.autorun
+class TestWhatever < MiniTest::Unit::TestCase
+  def test_truth
+    assert_equal(true, true)
+  end
+
+  #def test_...
+  #end
+end
